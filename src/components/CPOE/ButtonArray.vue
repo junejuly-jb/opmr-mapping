45962602a45ab5b9e5e7400e47af5cd9c85bee7e
June Aragoncillo <58539877+junejuly-jb@users.noreply.github.com>
2024-12-20 00:15:11 +0800

    added download

--- src/components/CPOE/ButtonArray.vue
+++ src/components/CPOE/ButtonArray.vue
@@ -1,26 +1,57 @@
 <script setup>
 import { useMappingStore } from '../../stores/mappings';
 import { mdiPlus, mdiFile, mdiCheck, mdiDownload } from '@mdi/js';
+import * as XLSX from "xlsx";
 
 const mappingStore = useMappingStore();
 const handleSave = () => {
   mappingStore.setConfirmationDialogText('Save','Do you want to save your changes to the OPMR mapping rules?')
   mappingStore.toggleConfirmationDialog(true)
 }
+
+const downloadExcel = () => {
+  //Flatten Data
+  const transformed = []
+  mappingStore.mappings.forEach(item => {
+    item.conditions.forEach((condition, conditionIndex) => {
+      condition.FortifierKey.forEach((additive, additiveIndex) => {
+        transformed.push({
+          "Feed-Base-Reference": (conditionIndex === 0 && additiveIndex === 0) ? item["productReference"] : "",
+          "Caloric-Rule": additiveIndex === 0 ? getCalories(condition["calories"]) : "",
+          "Product-Name": additiveIndex === 0 ? condition["reference"] : "",
+          "Additive": additive["fortifierKey"]
+        });
+      });
+    });
+  });
+
+  console.log(transformed)
+  // Convert JSON to a worksheet
+  const worksheet = XLSX.utils.json_to_sheet(transformed);
+
+  // Create a new workbook and append the worksheet
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Feed_Base");
+
+  // Generate Excel file and download
+  XLSX.writeFile(workbook, "mapping.xlsx");
+}
+
+const getCalories = (arr) => {
+  if(arr.length == 1){
+    return arr[0]
+  }
+  else if (!arr){
+    return '';
+  }
+  else {
+    return arr[0] + '-' + arr[arr.length - 1]
+  }
+}
 </script>
 <template>
     <div class="opmr__spacer__v">
       <span class="opmr__spacer__h"></span>
-      <!-- <v-btn
-        color="primary"
-        @click="mappingStore.setEmptyMapping"
-      >
-        Mapping Rule
-        <v-icon
-          :icon="mdiPlus"
-          end
-        ></v-icon>
-      </v-btn> -->
       <v-menu>
         <template v-slot:activator="{ props }">
           <v-btn
@@ -57,6 +88,7 @@ const handleSave = () => {
       <span class="opmr__spacer__h"></span>
       <v-btn
         color="primary"
+        @click="downloadExcel"
       >
         Download Mapping
         <v-icon
