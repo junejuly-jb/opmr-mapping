f32efde06bb76a1032588d01e66e27e7e3dc3ff5
June Aragoncillo <58539877+junejuly-jb@users.noreply.github.com>
2024-12-26 21:27:32 +0800

    updates

--- src/components/CPOE/ButtonArray.vue
+++ src/components/CPOE/ButtonArray.vue
@@ -26,8 +26,8 @@ const downloadExcel = () => {
   });
 
   console.log(transformed)
-  // Convert JSON to a worksheet
-  const worksheet = XLSX.utils.json_to_sheet(transformed);
+  
+  const worksheet = XLSX.utils.json_to_sheet(transformed); // Convert JSON to a worksheet
 
   // Create a new workbook and append the worksheet
   const workbook = XLSX.utils.book_new();
